5af5e58b76192793c26783ce57790117ebabd6b4
Evgenii Petukhov <petrovich2k@yandex.ru>
2020-09-26 20:20:52 +0300

    Added ChatComponent Added apiService, getChats method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
-import {
-  FacebookLoginProvider,
-} from 'angularx-social-login';
+import { FacebookLoginProvider } from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SocialLoginModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [
     {
---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Chat } from '../models/chat';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+  chats: Chat[];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getChats()
+      .subscribe(data => {
+        this.chats = data;
+      });
+  }
+
+}
