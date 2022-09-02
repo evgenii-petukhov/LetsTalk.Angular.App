c75b32838914cf6b5ab99ce6e9f0a6c88d238b4e
Evgenii Petukhov <petrovich2k@yandex.ru>
2022-09-02 22:57:15 +0200

    Rendered the ClockApp component in the Chat component

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -2,26 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Chat } from '../../models/rendering/chat';
 import { ChatMappingService } from '../../services/chat-mapping.service';
-import { Button } from 'src/react-component-library/cjs';
+import { ClockApp } from 'src/react-component-library/cjs';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss'],
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-  chats: Chat[];
+    chats: Chat[];
 
-  constructor(
-    private apiService: ApiService,
-    private chatMappingService: ChatMappingService
-  ) {}
+    constructor(
+        private apiService: ApiService,
+        private chatMappingService: ChatMappingService
+    ) {}
 
-  ngOnInit(): void {
-    this.apiService.getChats().subscribe((data) => {
-      this.chats = data.map(this.chatMappingService.map);
-    });
+    ngOnInit(): void {
+        this.apiService.getChats().subscribe((data) => {
+            this.chats = data.map(this.chatMappingService.map);
+        });
 
-    Button.renderExternally({ label: 'test', clickCallback: () => { console.log('btn clk'); }}, document.getElementById('bbb'));
-  }
+        ClockApp.renderExternally({ label: 'test', clickCallback: () => { console.log('ClockApp rendered'); }}, document.getElementById('bbb'));
+    }
 }
