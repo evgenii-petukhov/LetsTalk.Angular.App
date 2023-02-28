f337fa287d835250bedb53ecfa30d72af11be3ef
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-02-28 13:57:56 +0100

    Scroll down to the bottom of the chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+    QueryList,
+    ViewChild,
+    ViewChildren
+} from "@angular/core";
 import { Account } from "src/app/models/rendering/account";
 import { Message } from "src/app/models/rendering/message";
 import { ApiService } from '../../services/api.service';
@@ -8,14 +18,32 @@ import { ApiService } from '../../services/api.service';
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewInit {
     @Input() account: Account;
     @Input() messages: Message[];
     @Output() messageSentEvent = new EventEmitter<Message>();
+    //https://pumpingco.de/blog/automatic-scrolling-only-if-a-user-already-scrolled-the-bottom-of-a-page-in-angular/
+    @ViewChild('scrollFrame', { static: false }) scrollFrame: ElementRef;
+    @ViewChildren('scrollItem') itemElements: QueryList<any>;
     message = '';
 
-    constructor(private apiService: ApiService) {
+    private scrollContainer: any;
 
+    constructor(
+        private apiService: ApiService
+    ) { }
+
+    ngAfterViewInit() {
+        this.scrollContainer = this.scrollFrame.nativeElement;
+        this.itemElements.changes.subscribe(() => this.scrollToBottom());
+    }
+
+    private scrollToBottom(): void {
+        this.scrollContainer.scroll({
+            top: this.scrollContainer.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
     }
 
     send(): void {
