4bd57c4f139554f700cb6fe7c9cef46c0553f4d6
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-10 02:37:33 +0100

    SignalR auto reconnect + fixed failed build

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpTransportType, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import config from 'src/app/config';
+import { environment } from '../../environments/environment';
 import { IMessageDto } from '../api-client/api-client';
 import { TokenStorageService } from './token-storage.service';
 
@@ -12,13 +12,16 @@ export class SignalService {
     constructor(private tokenService: TokenStorageService) { }
 
     init(messageHandler: (message: IMessageDto) => void) {
-        const hubConnectionBuilder = new HubConnectionBuilder().withUrl(`${config.apiBaseUrl}/messagehub`, {
-            skipNegotiation: true,
-            transport: HttpTransportType.WebSockets
-        }).configureLogging(LogLevel.Information).build();
+        const hubConnectionBuilder = new HubConnectionBuilder()
+            .withUrl(`${environment.apiBaseUrl}/messagehub`, {
+                skipNegotiation: true,
+                transport: HttpTransportType.WebSockets
+            })
+            .withAutomaticReconnect(Array.from({length: 100}, x => 5))
+            .configureLogging(LogLevel.Information)
+            .build();
 
         hubConnectionBuilder.start().then(async () => {
-            console.log('Connection started.......!');
             await hubConnectionBuilder.invoke("Authorize", this.tokenService.getToken());
         }).catch(() => console.log('Error while connect with server'));
 
