83281f90b4139b23537b3f7cb570bd92b59dae1b
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-11 21:37:53 +0100

    Sort contacts by the number of unread messages, the by the date of the last message: set the LastMessageDate for new messages

--- src/app/pipes/orderby.ts
+++ src/app/pipes/orderby.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { List, ListIteratee, Many, orderBy } from 'lodash';
+import { get, List, Many, orderBy } from 'lodash';
 
 @Pipe({
     name: "orderBy"
 })
 export class OrderByPipe<T> implements PipeTransform {
-    transform(array: List<T>, sortBy: Many<ListIteratee<T>>, order: Many<boolean | "asc" | "desc">): any[] {
-        return orderBy(array, sortBy, order);
+    transform(array: List<T>, sortBy: string[], order: Many<boolean | "asc" | "desc">): any[] {
+        return orderBy(array, (item) => sortBy.map(x => get(item, x, 0)), order);
     }
 }
