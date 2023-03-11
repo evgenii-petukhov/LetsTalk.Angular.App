fa4210598c93a5a264bd955986fc1ae3e4808c18
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-11 16:28:53 +0100

    Linux: added a new (-b) parameter to rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,14 @@
+branch="master"
+while getopts b: args
+do
+    case "${args}" in
+        b) branch=${OPTARG};;
+    esac
+done
+
 git reset --hard
-git checkout origin/master
-git pull origin master
+git fetch origin
+git checkout origin/${branch}
+git pull origin ${branch}
 npm install --quiet
 npm run build:dev:hosting
