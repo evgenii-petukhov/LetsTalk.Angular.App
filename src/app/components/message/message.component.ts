09f78f9b447f48297b3fe55038a18fe8c524022e
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-13 22:42:35 +0100

    Passed DateCreated as Unix time: removed the DateTimeService

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Message } from 'src/app/models/message';
-import { DateTimeService } from 'src/app/services/date-time.service';
 
 @Component({
     selector: 'app-message',
@@ -10,9 +9,4 @@ import { DateTimeService } from 'src/app/services/date-time.service';
 export class MessageComponent {
     @Input() message: Message;
 
-    timezone = this.dateTimeService.getLocalTimezone();
-
-    constructor(
-        private dateTimeService: DateTimeService
-    ) {}
 }
