cf4e082c34e251518fa2fcf4b70c8efeb0d6e996
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-29 23:45:37 +0200

    Notifications: use ServiceWorker

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -6,14 +6,31 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NotificationService {
 
-    constructor(private toastr: ToastrService) { }
+    private isServiceWorkerRegistered = false;
+
+    constructor(private toastr: ToastrService) {
+        navigator.serviceWorker.register("sw.js").then(() => {
+            this.isServiceWorkerRegistered = true;
+            console.log('serviceWorker success');
+        }).catch(() => {
+            console.log('serviceWorker error');
+        });
+    }
 
     showNotification(title: string, message: string, isToastAllowed: boolean): void {
         Notification.requestPermission().then(permission => {
             if (permission === 'granted') {
-                new Notification(title, {
-                    body: message
-                });
+                if (this.isServiceWorkerRegistered) {
+                    navigator.serviceWorker.ready.then(function (registration) {
+                        registration.showNotification(title, {
+                            body: message
+                        });
+                    })
+                } else {
+                    new Notification(title, {
+                        body: message
+                    });
+                }
             } else if (isToastAllowed) {
                 this.toastr.info(message, title);
             }
