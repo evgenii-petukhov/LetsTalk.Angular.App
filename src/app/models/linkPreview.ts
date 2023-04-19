b0d3451032bfc3ad25d0b5b7a0cefcd29e9011c7
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-04-20 01:04:16 +0200

    Added missing type definition

--- src/app/models/linkPreview.ts
+++ src/app/models/linkPreview.ts
@@ -1,10 +1,12 @@
+import { LinkPreviewDto } from '../api-client/api-client';
+
 export class LinkPreview {
     accountId?: number;
     title?: string | undefined;
     imageUrl?: string | undefined;
     url?: string | undefined;
 
-    constructor(init?: Partial<LinkPreview>) {
+    constructor(init?: Partial<LinkPreview | LinkPreviewDto>) {
         Object.assign(this, init);
     }
 }
