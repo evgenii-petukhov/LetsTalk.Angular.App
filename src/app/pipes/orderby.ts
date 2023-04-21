8910ccac56611ff9d5acc09114d59257701022de
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-04-21 05:36:22 +0200

    Fixed build warnings: lodash, ES2022

--- src/app/pipes/orderby.ts
+++ src/app/pipes/orderby.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { List, ListIteratee, Many, orderBy } from 'lodash';
+import { List, ListIteratee, Many, orderBy } from 'lodash-es';
 
 @Pipe({
     name: 'orderBy'
