000114ac320b66340ac61416ae3916ee05b353da
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-05-31 00:38:14 +0200

    Profile: added the Avatar component

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IAccountDto } from 'src/app/api-client/api-client';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'app-avatar',
     templateUrl: './avatar.component.html',
     styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent implements OnInit {
-    @Input() account: IAccountDto;
+export class AvatarComponent implements OnChanges {
+    @Input() urlOptions: string[];
     photoUrl: string;
 
-    ngOnInit(): void {
-        this.photoUrl = this.account.photoUrl ?? 'images/empty-avatar.svg';
+    ngOnChanges(): void {
+        if (this.urlOptions) {
+            const photoUrl = this.urlOptions.find(url => url);
+            if (photoUrl) {
+                this.photoUrl = photoUrl;
+                return;
+            }
+        }
+        this.photoUrl = 'images/empty-avatar.svg';
     }
 }
