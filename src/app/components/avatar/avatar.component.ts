6ecb608fca262b1c56afc66e9b7ae2e4e0baf850
Evgenii Petukhov <petukhoven@gmail.com>
2023-06-11 02:48:20 +0200

    Profile: add default avatar, if picture url is not valid anymore, optimizations

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -8,8 +8,9 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class AvatarComponent implements OnChanges {
     @Input() urlOptions: (string | number)[];
-    photoUrl: string;
+    backgroundImage: string;
     private defaultPhotoUrl = 'images/empty-avatar.svg';
+    private base64Regex = /data:image\/(jpeg|png|gif){1};base64,([^\\"]*)/g;
 
     constructor(
         private apiservice: ApiService
@@ -17,8 +18,20 @@ export class AvatarComponent implements OnChanges {
 
     ngOnChanges(): void {
         this.urlOptions = this.urlOptions ?? [];
-        this.urlOptions.push(this.defaultPhotoUrl);
-        const promises = this.urlOptions.filter(url => url).map(url => Number(url)
+        this.urlOptions = this.urlOptions.filter(url => url);
+        if (this.urlOptions.length === 0) {
+            this.setBackgroundImage(this.defaultPhotoUrl);
+            return;
+        }
+
+        this.removeElementsAfterBase64(this.urlOptions);
+
+        if (this.isBase64(this.urlOptions[0])) {
+            this.setBackgroundImage(this.urlOptions[0] as string);
+            return;
+        }
+
+        const promises = this.urlOptions.map(url => Number(url)
             ? new Promise<string>((resolve) => {
                 this.apiservice.getImage(url as number).subscribe({
                     next: imageDto => {
@@ -32,10 +45,31 @@ export class AvatarComponent implements OnChanges {
             : Promise.resolve<string>(url as string));
 
         Promise.allSettled(promises).then((results) => {
-            this.photoUrl = results
+            const urls = results
                 .filter((result: PromiseFulfilledResult<string>) => result.status === 'fulfilled' && result.value)
                 .map((result: PromiseFulfilledResult<string>) => result.value)
-                .find(url => url);
+                .filter(url => url);
+
+            urls.push(this.defaultPhotoUrl);
+
+            this.removeElementsAfterBase64(urls);
+
+            this.setBackgroundImage(...urls);
         });
+    }
+
+    private isBase64(value: (string | number)): boolean {
+        return !Number(value) && !!(value as string).match(this.base64Regex);
+    }
+
+    private removeElementsAfterBase64(array: (string | number)[]): void {
+        const base64Index = array.findIndex(x => this.isBase64(x));
+        if (base64Index > -1) {
+            array.length = base64Index + 1;
+        }
+    }
+
+    private setBackgroundImage(...urls: string[]) {
+        this.backgroundImage = urls.map(url => `url('${url}')`).join(', ');
     }
 }
