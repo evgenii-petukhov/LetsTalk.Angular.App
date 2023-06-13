1e9d34623b4fbefd22d703ead8783bd714e374d6
Evgenii Petukhov <petukhoven@gmail.com>
2023-06-13 04:04:39 +0200

    Self-hosted: fixed configs to run applications without IIS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     facebookAppId: '1391147151094704',
     vkAppId: '7615545',
-    apiBaseUrl: 'https://api.letstalk.local',
-    notificationServiceUrl: 'https://notifications.letstalk.local/messagehub',
+    apiBaseUrl: 'https://localhost:5000',
+    notificationServiceUrl: 'http://127.0.0.1:5002/messagehub',
     notificationServiceReconnectInterval: 5000,
     avatarMaxWidth: 512,
     avatarMaxHeight: 512
