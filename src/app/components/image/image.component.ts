b5ed5cede8d4227923628df554de5531feca64ac
Evgenii Petukhov <petukhoven@gmail.com>
2023-07-14 16:16:33 +0200

    Send picture: fixed "NG0100: Expression has changed after it was checked" on isLoading changed

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,29 +1,30 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { StoreService } from 'src/app/services/store.service';
 
 @Component({
     selector: 'app-image',
     templateUrl: './image.component.html',
     styleUrls: ['./image.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ImageComponent implements OnChanges {
+export class ImageComponent implements OnInit {
     @Input() imageId: number;
     url: string;
     isLoading = true;
 
     constructor(
         private storeService: StoreService,
+        private cdr: ChangeDetectorRef
     ) { }
 
-    ngOnChanges(): void {
+    ngOnInit(): void {
         if (!this.imageId) {
             return;
         }
-
-        this.isLoading = true;
         this.storeService.getImageContent(this.imageId).then(content => {
             this.url = content;
             this.isLoading = false;
+            this.cdr.detectChanges();
         }).catch(e => {
             console.error(e);
         });
