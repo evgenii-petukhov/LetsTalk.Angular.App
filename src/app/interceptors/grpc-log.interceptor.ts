1e09ca1ed8b22f4a93f96f880476d9730df45c58
Evgenii Petukhov <petukhoven@gmail.com>
2023-08-09 08:07:00 +0200

    http / grpc request logging: added `downloaded`, `uploaded`

--- src/app/interceptors/grpc-log.interceptor.ts
+++ src/app/interceptors/grpc-log.interceptor.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GrpcLogInterceptor {
-    async intercept(request: { c: { name: any; }; }, invoker: (arg0: any) => Promise<any>) {
+    async intercept(request: { f: { array: string | any[]; }; c: { name: any; }; }, invoker: (arg0: any) => Promise<any>) {
         const t0 = performance.now();
         const response = await invoker(request);
-        console.log(`[gRPC] ${request.c.name} ${(performance.now() - t0).toFixed(0)}ms`);
+        const downloaded = response.c.array && response.c.array.length && response.c.array[0].length ? ' ▼' + (response.c.array[0].length / 1024).toFixed() + 'kB' : '';
+        const uploaded = request.f.array && request.f.array.length && request.f.array[0].length ? ' ▲' + (request.f.array[0].length / 1024).toFixed() + 'kB' : '';
+        console.log(`[gRPC] ${request.c.name} ${(performance.now() - t0).toFixed()}ms${downloaded}${uploaded}`);
         return response;
     };
 }
--- src/app/interceptors/http-log.interceptor.ts
+++ src/app/interceptors/http-log.interceptor.ts
@@ -6,8 +6,10 @@ import { Observable, filter, tap } from 'rxjs';
 export class HttpLogInterceptor implements HttpInterceptor {
     intercept<T>(request: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>> {
         const t0 = performance.now();
-        return next.handle(request).pipe(filter(response => response instanceof HttpResponse), tap(() => {
-            console.log(`[${request.method}] ${request.url} ${(performance.now() - t0).toFixed(0)}ms`);
+        return next.handle(request).pipe(filter(response => response instanceof HttpResponse), tap((response: HttpResponse<T>) => {
+            const downloaded = response.body && response.body instanceof Blob ? ` ▼${(response.body.size / 1024).toFixed()}kB` : '';
+            const uploaded = request.body && typeof request.body === 'string' ? ` ▲${(request.body.length / 1024).toFixed()}kB` : '';
+            console.log(`[${request.method}] ${request.url} ${(performance.now() - t0).toFixed(0)}ms${downloaded}${uploaded}`);
         }));
     }
 }
