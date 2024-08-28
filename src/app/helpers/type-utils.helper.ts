97fe17da66136a21be5d2b29a913bd6c97e37a07
Evgenii Petukhov <petukhoven@gmail.com>
2024-08-28 18:55:33 +0200

    Login by email: covered the type-utils.helper with unit tests

--- src/app/helpers/type-utils.helper.ts
+++ src/app/helpers/type-utils.helper.ts
@@ -1,2 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const isOfType = <T extends new (...args: any[]) => any>(obj: any, type: T): obj is T => obj instanceof type;
+export const isOfType = <T extends new (...args: any[]) => any>(obj: any, type: T): obj is T => {
+    // Ensure 'type' is a function and a constructor
+    return typeof type === 'function' && type.prototype instanceof Object && obj instanceof type;
+};
