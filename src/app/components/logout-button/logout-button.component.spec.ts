6d36b481d97a7784edd6e96430f756a90a09d9bb
Evgenii Petukhov <petukhoven@gmail.com>
2024-09-21 01:41:37 +0200

    Added unit tests for the LogoutButton component

--- src/app/components/logout-button/logout-button.component.spec.ts
+++ src/app/components/logout-button/logout-button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LogoutButtonComponent } from './logout-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { By } from '@angular/platform-browser';
 
 describe('LogoutButtonComponent', () => {
     let component: LogoutButtonComponent;
@@ -9,14 +10,40 @@ describe('LogoutButtonComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [LogoutButtonComponent],
+            imports: [FontAwesomeModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(LogoutButtonComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should display the fa-right-from-bracket icon', () => {
+        // Arrange
+
+        // Act
+        fixture.detectChanges();
+
+        // Assert
+        const icon = fixture.debugElement.query(By.css('fa-icon'));
+        expect(icon).toBeTruthy();
+        expect(icon.componentInstance.icon).toEqual(
+            component.faRightFromBracket,
+        );
+    });
+
+    it('should emit the buttonClick event when the button is clicked', () => {
+        // Arrange
+        spyOn(component.buttonClick, 'emit');
+
+        // Act
+        const button = fixture.debugElement.query(By.css('a')).nativeElement;
+        button.click();
+
+        // Assert
+        expect(component.buttonClick.emit).toHaveBeenCalled();
     });
 });
