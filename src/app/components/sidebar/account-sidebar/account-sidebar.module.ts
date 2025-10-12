c0e2d0315cc169a2c55d8b052f9cbd61b3a7f414
Evgenii Petukhov <petukhoven@gmail.com>
2025-10-12 02:19:11 +0200

    Moved AccountList component to AccountSidebar module

--- src/app/components/sidebar/account-sidebar/account-sidebar.module.ts
+++ src/app/components/sidebar/account-sidebar/account-sidebar.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountListItemComponent } from './account-list-item/account-list-item.component';
 import { SharedModule } from '../../shared/shared.module';
+import { AccountListComponent } from './account-list/account-list.component';
 
 @NgModule({
-    declarations: [AccountListItemComponent],
+    declarations: [AccountListItemComponent, AccountListComponent],
     imports: [CommonModule, SharedModule],
-    exports: [AccountListItemComponent],
+    exports: [AccountListItemComponent, AccountListComponent],
 })
 export class AccountSidebarModule {}
--- src/app/components/sidebar/sidebar.module.ts
+++ src/app/components/sidebar/sidebar.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { AccountListComponent } from './account-list/account-list.component';
 import { AccountSidebarNavigationPanelComponent } from './account-sidebar-navigation-panel/account-sidebar-navigation-panel.component';
 import { AccountSidebarComponent } from './account-sidebar/account-sidebar.component';
 import { SidebarComponent } from './sidebar.component';
@@ -12,7 +11,6 @@ import { AccountSidebarModule } from './account-sidebar/account-sidebar.module';
 
 @NgModule({
     declarations: [
-        AccountListComponent,
         AccountSidebarNavigationPanelComponent,
         AccountSidebarComponent,
         SidebarComponent,
